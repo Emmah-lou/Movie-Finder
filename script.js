607afe9df4e61a3792384e16dc8ab30bc189f194
Emma Lou Who <122497149+Emmah-lou@users.noreply.github.com>
2023-03-09 16:24:40 -0700

    changed to SCSS also have movie finder and api callback working correctly

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-var httpRequest = new XMLHttpRequest();           
-httpRequest.onload = function() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      //console.log(httpRequest.responseText);
-      var response = JSON.parse(httpRequest.responseText);
-      console.log(response);
-      //document.getElementById('poster').src = response.Search[0].Poster;
-      for (let i = 0; i < response.Search.length; i++) {
-        
-        const movie = response.Search[i];
-        var results = $('div');
-        results.append(
-          '<img src="' + movie.Poster + '" alt="movie poster">' +
-          '<h3>Title: ' + movie.Title + '</h3>' +
-          '<p>' + movie.Year + '</p>' +
-          '<p>' + movie.Type + '</p>'
-        );
-      }
-      
-    } else {
-      console.log(httpRequest.statusText);
-    }
-  }
-}
-httpRequest.onerror = function() {
-  console.log(httpRequest.statusText);
-}
 var searchMovies = function (e) {
   e.preventDefault();
-  var searchResult = document.querySelector("input").value;
+  var searchResult = $("input").val();
   
   if (searchResult) {
+    var httpRequest = new XMLHttpRequest();           
+    httpRequest.onload = function() {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          var response = JSON.parse(httpRequest.responseText);
+          console.log(response);
+          var results = $('.movie-container');
+          results.empty();
+          for (let i = 0; i < response.Search.length; i++) {
+            const movie = response.Search[i];
+            results.append(
+              '<div class="movieItem">' +
+              '<img class="img-fluid" src="' + movie.Poster + '" alt="~Movie Poster Not Found~">' +
+              '<h3>Title: ' + movie.Title + '</h3>' +
+              '<p>Year: ' + movie.Year + '</p>' +
+              '<p>Type: ' + movie.Type + '</p>' +
+              '</div>'
+            );
+          }
+        } else {
+          console.log(httpRequest.statusText);
+        }
+      }
+    };
+    httpRequest.onerror = function() {
+      console.log(httpRequest.statusText);
+    };
     httpRequest.open('GET', 'https://www.omdbapi.com/?s=' + searchResult + '&plot=short&apikey=73740781');
     httpRequest.send(null);
-    
   }
-}
+};
